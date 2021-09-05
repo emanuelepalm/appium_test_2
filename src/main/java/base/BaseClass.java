de25c78fe57f20366871dcf436b8388debf50b80
EmanuelePalm <emanuele.palmieri@aesys.tech>
2021-09-05 15:15:20 +0200

    fix

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -31,7 +31,8 @@ public class BaseClass {
 
             server = new AppiumServer();
             Thread.sleep(5000);
-
+            AndroidEmu.launchEmulator("Pixel4J");
+            Thread.sleep(5000);
             DesiredCapabilities cap=new DesiredCapabilities();
             cap.setCapability("deviceName", emuName);
             cap.setCapability("udid", udid);
